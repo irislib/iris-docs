8f5f217924b9eaf0c2464eb1ccbdea5f8d2ed664
Martti Malmi <sirius@iki.fi>
2024-04-30 12:41:57 +0300

    v1.0.5, fix login

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import { ndk } from 'irisdb-nostr';
 import ReactDOM from 'react-dom/client';
 
 import { App } from '@/app';
 import config from '@/config.json';
+
+ndk(); // init NDK & irisdb login flow
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
 
